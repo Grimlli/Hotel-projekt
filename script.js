f9469dedda441f5ed84784c8b16da2f5f7fdc490
Grimlli <adam.hedberg.ahj@gmail.com>
2023-01-07 15:00:01 +0100

    pusch 1/2 of day

--- script.js
+++ script.js
@@ -15,6 +15,8 @@ const room3Open = document.getElementById("room3-open");
 const room3Info = document.getElementById("room3-info");
 
 const send = document.getElementById("send").addEventListener("click", formDone);
+
+
 /*
 const runway = document.getElementById("runway");
 const cForm = document.getElementById("customer-form");
@@ -45,13 +47,57 @@ function openRoomText3(){
 }
 
 
+
 /*Form Done temp solution*/
 function formDone(){
-
-    console.log("JAg kom hit");
-    formMessage.classList.toggle("active");
+    console.log("FormDone");
+   
+    if(requierd() == true){
+        console.log(requierd);
+        formMessage.classList.toggle("active");
+        send.classList.toggle("after");
     
+    }
+
 }
+
+
+function requierd(){
+
+    const email = document.getElementById("email").value;
+    var name = document.getElementById("name").value;
+    var phone = document.getElementById("phone").value;
+    var customerStatus = document.getElementById("customer-status").value;
+    var message = document.getElementById("message").value;
+    console.log("Innna if sats");
+  
+  
+    if(typeof email !="undefiend" && email.includes("@")){
+        console.log("Kom igenom email");
+
+        if(typeof name != ""){
+            console.log("Kom igenom name");
+
+            if(typeof phone !="undefiend"){
+                console.log("Kom igenom phone");
+
+                if(typeof customerStatus !="undefiend"){
+                console.log("Kom igenom CustomerStatus");
+
+                    if(typeof message !="undefiend"){
+                        console.log("Kom igenom message");
+                        return true;
+                    }
+                }
+            }
+        }
+    }
+    else{
+         return false;
+        }
+
+}
+
 
     
 hamburger.addEventListener("click",openHamburger);
